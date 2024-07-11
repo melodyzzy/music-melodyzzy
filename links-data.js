0c37353c8487f2edecda0322ada21652fd33a3f6
melodyzzy <3096646092@qq.com>
2024-07-11 22:59:16 +0800

    Initial website upload

---
+++ links-data.js
@@ -0,0 +1,12 @@
+const linksData = {
+    'qq': [
+        { name: '3 Strikes(Explicit)', url: '3 Strikes(Explicit).mp3' }
+    ],
+    'netease': [
+        { name: '与我无关', url: '与我无关.mp3' }
+    ],
+    'youtube': [
+        { name: 'Meant To Be', url: 'Meant To Be.mp3' },
+        { name: 'I Took A Pill In Ibiza (Seeb Remix)', url: 'I Took A Pill In Ibiza (Seeb Remix).mp3' }
+    ]
+};
