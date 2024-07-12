a3cf006cac3801a6a77243eb44fd850a6ba8ba3a
melodyzzy <3096646092@qq.com>
2024-07-12 09:37:07 +0800

    Add new music files and update links-data.js

--- links-data.js
+++ links-data.js
@@ -1,9 +1,15 @@
 const linksData = {
     'qq': [
-        { name: '3 Strikes(Explicit)', url: '3 Strikes(Explicit).mp3' }
+        { name: '3 Strikes(Explicit)', url: '3 Strikes(Explicit).mp3' },
+        { name: 'Way Back Home', url: 'Way Back Home.mp3' }
     ],
     'netease': [
-        { name: '与我无关', url: '与我无关.mp3' }
+        { name: '与我无关', url: '与我无关.mp3' },
+        { name: '与我无关（释怀版）', url: '与我无关（释怀版）.mp3'},
+        { name: '晚安', url: '晚安.mp3' },
+        { name: 'MOM', url: 'MOM.mp3' },
+        { name: '还是分开', url: '还是分开.mp3' },
+        { name: '你走之后1.0', url: '你走之后1.0.mp3'}
     ],
     'youtube': [
         { name: 'Meant To Be', url: 'Meant To Be.mp3' },
