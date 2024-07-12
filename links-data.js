c34145f1c68c8dacc866cd9b9f6238d6777e7092
melodyzzy <3096646092@qq.com>
2024-07-12 13:22:53 +0800

    Updated links-data.js with new links

--- links-data.js
+++ links-data.js
@@ -1,6 +1,14 @@
 const linksData = {
     'qq': [
         { name: '3 Strikes(Explicit)', url: '3 Strikes(Explicit).mp3' },
+        { name: '冬眠·2023', url: '冬眠·2023.mp3' },
+        { name: '如果爱忘了(Live)', url: '如果爱忘了(Live).mp3' },
+        { name: '我想念', url: '我想念.mp3' },
+        { name: '兰亭序', url: '兰亭序.mp3' },
+        { name: '起风了', url: '起风了.mp3' },
+        { name: '光年之外', url: '光年之外.mp3' },
+        { name: 'All For Love', url: 'All For Love.mp3' },
+        { name: 'Blue', url: 'Blue.mp3' },
         { name: 'Way Back Home', url: 'Way Back Home.mp3' }
     ],
     'netease': [
