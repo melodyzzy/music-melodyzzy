3043a7fb36d5667cb35f57f27963ae56ca5abcad
melodyzzy <3096646092@qq.com>
2024-07-12 19:09:55 +0800

    Remove old music files and update links-data.js

--- links-data.js
+++ links-data.js
@@ -1,28 +1,28 @@
 const linksData = {
     'qq': [
-        { name: '3 Strikes(Explicit)', url: '3 Strikes(Explicit).mp3' },
-        { name: '冬眠·2023', url: '冬眠·2023.mp3' },
-        { name: '如果爱忘了(Live)', url: '如果爱忘了(Live).mp3' },
-        { name: '一程山水', url: '一程山水.mp3' },
-        { name: '我想念', url: '我想念.mp3' },
-        { name: '兰亭序', url: '兰亭序.mp3' },
-        { name: '起风了', url: '起风了.mp3' },
-        { name: '光年之外', url: '光年之外.mp3' },
-        { name: 'All For Love', url: 'All For Love.mp3' },
-        { name: 'Blue', url: 'Blue.mp3' },
-        { name: 'Way Back Home', url: 'Way Back Home.mp3' }
+        { name: '3 Strikes(Explicit)', url: 'qqmusic/3 Strikes(Explicit).mp3' },
+        { name: '冬眠·2023', url: 'qqmusic/冬眠·2023.mp3' },
+        { name: '如果爱忘了(Live)', url: 'qqmusic/如果爱忘了(Live).mp3' },
+        { name: '一程山水', url: 'qqmusic/一程山水.mp3' },
+        { name: '我想念', url: 'qqmusic/我想念.mp3' },
+        { name: '兰亭序', url: 'qqmusic/兰亭序.mp3' },
+        { name: '起风了', url: 'qqmusic/起风了.mp3' },
+        { name: '光年之外', url: 'qqmusic/光年之外.mp3' },
+        { name: 'All For Love', url: 'qqmusic/All For Love.mp3' },
+        { name: 'Blue', url: 'qqmusic/Blue.mp3' },
+        { name: 'Way Back Home', url: 'qqmusic/Way Back Home.mp3' }
     ],
     'netease': [
-        { name: '与我无关', url: '与我无关.mp3' },
-        { name: '与我无关（释怀版）', url: '与我无关（释怀版）.mp3'},
-        { name: '晚安', url: '晚安.mp3' },
-        { name: 'MOM', url: 'MOM.mp3' },
-        { name: '还是分开', url: '还是分开.mp3' },
-        { name: '你走之后1.0', url: '你走之后1.0.mp3'}
+        { name: '与我无关', url: 'neteasemusic/与我无关.mp3' },
+        { name: '与我无关（释怀版）', url: 'neteasemusic/与我无关（释怀版）.mp3'},
+        { name: '晚安', url: 'neteasemusic/晚安.mp3' },
+        { name: 'MOM', url: 'neteasemusic/MOM.mp3' },
+        { name: '还是分开', url: 'neteasemusic/还是分开.mp3' },
+        { name: '你走之后1.0', url: 'neteasemusic/你走之后1.0.mp3'}
     ],
     'youtube': [
-        { name: 'Meant To Be', url: 'Meant To Be.mp3' },
-        { name: 'Diamonds', url: 'Diamonds.mp3' },
-        { name: 'I Took A Pill In Ibiza (Seeb Remix)', url: 'I Took A Pill In Ibiza (Seeb Remix).mp3' }
+        { name: 'Meant To Be', url: 'youtubemusic/Meant To Be.mp3' },
+        { name: 'Diamonds', url: 'youtubemusic/Diamonds.mp3' },
+        { name: 'I Took A Pill In Ibiza (Seeb Remix)', url: 'youtubemusic/I Took A Pill In Ibiza (Seeb Remix).mp3' }
     ]
 };
